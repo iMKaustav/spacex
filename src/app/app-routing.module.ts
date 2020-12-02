c6d4037d75bd2b9fe16660cc64f216822044b452
Kaustav Das <kaustav.das@xyleminc.com>
2020-12-03 05:20:00 +0530

    routing configuration

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'launch-programs',
+    loadChildren: () => import('./launch-programs/launch-programs.module').then(m => m.LaunchProgramsModule)
+  },
+  { path: '**', redirectTo: 'launch-programs' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/launch-programs/launch-programs.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { LaunchListComponent } from './launch-list/launch-list.component';
+import { RouterModule, Routes } from '@angular/router';
+
+const launchRoutes: Routes = [
+  { path: '', component: LaunchListComponent }
+];
+@NgModule({
+  declarations: [
+    LaunchListComponent
+  ],
+  imports: [
+    CommonModule,
+    RouterModule.forChild(launchRoutes)
+  ]
+})
+export class LaunchProgramsModule { }
